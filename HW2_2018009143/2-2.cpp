3f65e5f91ab5c7def135c859908f7df09af0bef7
2018009143 <jhcse5189@gmail.com>
2019-10-17 07:31:09 +0900

    init. 2-2.cpp

--- HW2_2018009143/2-2.cpp
+++ HW2_2018009143/2-2.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 
-// Rod Cutting.
+/* Rod Cutting. (ch 15.1) */
+
+int main(int argc, char* argv[]) {
+
+    int rodLen;
+    // TODO: replace dynamic alloc. by calc.
+    int values[5];
+    std::string inpath = "input2-2.txt";
+
+    /* INPUT */
+    // line 1. length of rod
+    // line 2. value of each length.
+
+    // read File
+    std::ifstream inf(inpath);
+
+    if (inf) {
+        inf >> rodLen;
+        for (i = 0; !inf.eof(); i++)
+            inf >> values[i];
+    }
+
+    /* OUTPUT */
+    // line 1. When Maximum profit,
+    // line 2. lengths of rods order by asc;
+
+
+}
